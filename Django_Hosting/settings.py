35fd02f5b6da0b90b7fc516493abd636a633804b
priyanevon <priyanevon21@gmail.com>
2022-09-01 18:18:21 +0530

    adding files

--- Django_Hosting/settings.py
+++ Django_Hosting/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '$@_u=e0v-fp9=(%8!@#df^*int037o0v*h-@ywkcar&d@$t$f7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pythondemo.azurewebsites.net']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATIC_ROOT = (os.path.join(BASE_DIR,'static'))
 
