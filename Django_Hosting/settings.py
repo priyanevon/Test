06d31a20d59cbac35de37b1e34c453b0823fbc59
priyanevon <priyanevon21@gmail.com>
2022-09-01 16:27:02 +0530

    adding files

--- Django_Hosting/settings.py
+++ Django_Hosting/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-'''STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'App_Django_Hosting/static')
-]'''
+STATIC_ROOT = (os.path.join(BASE_DIR,'static'))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 '''
 STATIC_ROOT = (os.path.join(BASE_DIR,'assets'))
 
