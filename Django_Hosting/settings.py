1d1784c91cdf0353bfba0234fd57ab7870030c2a
priyanevon <priyanevon21@gmail.com>
2022-09-01 18:40:43 +0530

    adding files

--- Django_Hosting/settings.py
+++ Django_Hosting/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR/'static'
+'''STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATIC_ROOT = (os.path.join(BASE_DIR,'static'))
 
@@ -132,7 +135,7 @@ STATIC_ROOT = (os.path.join(BASE_DIR,'assets'))
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = (os.path.join(BASE_DIR,'media'))
-
+'''
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
